ef1f94d1340c4db8e30cb75da45699b4b3947595
Zakk Hoyt <zakk@threefoldphotos.com>
2014-03-10 11:57:15 -0700

    Initial Commit

---
+++ RC Video/VWWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VWWAppDelegate.h
+//  RC Video
+//
+//  Created by Zakk Hoyt on 3/10/14.
+//  Copyright (c) 2014 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VWWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RC Video/VWWViewController.h
@@ -0,0 +1,13 @@
+//
+//  VWWViewController.h
+//  RC Video
+//
+//  Created by Zakk Hoyt on 3/10/14.
+//  Copyright (c) 2014 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VWWViewController : UIViewController
+
+@end
